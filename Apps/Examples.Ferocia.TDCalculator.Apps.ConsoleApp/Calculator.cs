533cc7c3bcd578427920a89441a6c6662539528b
Muhammad Siddiqui <Muhammad.Siddiqui@civica.com.au>
2025-06-23 16:16:25 +1000

    Getting console inputs from the user

--- Apps/Examples.Ferocia.TDCalculator.Apps.ConsoleApp/Calculator.cs
+++ Apps/Examples.Ferocia.TDCalculator.Apps.ConsoleApp/Calculator.cs
@@ -22,7 +22,34 @@ namespace Examples.Ferocia.TDCalculator.Apps.ConsoleApp
         /// Main calculator method that will be executed from Program
         /// </summary>
         public void Run()
-        { 
+        {
+            Console.WriteLine("Enter the starting deposit amout (e.g.$10,000) and press [enter]. Please enter only numbers without the $ sign.");
+            var isValid = double.TryParse(Console.ReadLine(), out double initialAmount);
+
+            while (!isValid)
+            {
+                Console.WriteLine("Invalid initial deposit. Please enter the starting deposit amout (e.g.$10,000) and press [enter]. Please enter only numbers without the $ sign.");
+                isValid = double.TryParse(Console.ReadLine(), out initialAmount);
+            }
+
+            Console.WriteLine("Enter the interest rate (e.g. 1.10) and press [enter]. Please enter without the % sign.");
+            isValid = double.TryParse(Console.ReadLine(), out double interestRate);
+
+            while (!isValid)
+            {
+                Console.WriteLine("Invalid interest rate. Please enter the interest rate (e.g. 1.10) and press [enter]. Please enter without the % sign.");
+                isValid = double.TryParse(Console.ReadLine(), out interestRate);
+            }
+
+            Console.WriteLine("Enter the investment term (e.g. 3 years) and press [enter]. Please enter only the number of years as a number.");
+            isValid = int.TryParse(Console.ReadLine(), out int investmentTerm);
+
+            while (!isValid)
+            {
+                Console.WriteLine("Invalid investment term. Please enter the investment term (e.g. 3 years) and press [enter]. Please enter only the number of years as a number.");
+                isValid = int.TryParse(Console.ReadLine(), out investmentTerm);
+            }
+            
         }
     }
 }
