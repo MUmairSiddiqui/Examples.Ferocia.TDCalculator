bd131d4a3f73534a74a0920210ab6e2b121b51c0
Muhammad Siddiqui <Muhammad.Siddiqui@civica.com.au>
2025-06-23 16:06:47 +1000

    Calculator class added

---
+++ Apps/Examples.Ferocia.TDCalculator.Apps.ConsoleApp/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using Examples.Ferocia.TDCalculator.Domain.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.Ferocia.TDCalculator.Apps.ConsoleApp
+{
+    internal class Calculator(
+        IConfiguration configuration,
+        ILogger<Calculator> logger,
+        ICompoundInterestCalculatorService calculatorService)
+    {
+        private readonly IConfiguration _configuration = configuration;
+        private readonly ILogger<Calculator> _logger = logger;
+        private readonly ICompoundInterestCalculatorService _calculatorService = calculatorService;
+
+        /// <summary>
+        /// Main calculator method that will be executed from Program
+        /// </summary>
+        public void Run()
+        { 
+        }
+    }
+}
--- Apps/Examples.Ferocia.TDCalculator.Apps.ConsoleApp/Program.cs
+++ Apps/Examples.Ferocia.TDCalculator.Apps.ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Examples.Ferocia.TDCalculator.Apps.ConsoleApp;
 using Examples.Ferocia.TDCalculator.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,9 +14,14 @@ var configuration = new ConfigurationBuilder()
 var serviceProvider = new ServiceCollection()
     .AddSingleton<IConfiguration>(configuration)
     .AddLogging(builder => builder.AddConsole())
+    .AddSingleton<Calculator>()
     .ConfigureServices()//Examples.Ferocia.TDCalculator.Services DI
     .BuildServiceProvider();
 
 Console.WriteLine("Calculator started");
+
+var calculator = serviceProvider.GetRequiredService<Calculator>();
+calculator.Run();
+
 Console.Write("Press any key to exit.");
 Console.ReadKey();
