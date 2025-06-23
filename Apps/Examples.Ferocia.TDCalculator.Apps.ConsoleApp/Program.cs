4731b04fbd81de2750740901f492ec2f754194b9
Muhammad Siddiqui <Muhammad.Siddiqui@civica.com.au>
2025-06-23 16:01:50 +1000

    Console app added with standard DI into service collection

---
+++ Apps/Examples.Ferocia.TDCalculator.Apps.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Examples.Ferocia.TDCalculator.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddCommandLine(args)
+    .Build();
+
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<IConfiguration>(configuration)
+    .AddLogging(builder => builder.AddConsole())
+    .ConfigureServices()//Examples.Ferocia.TDCalculator.Services DI
+    .BuildServiceProvider();
+
+Console.WriteLine("Calculator started");
+Console.Write("Press any key to exit.");
+Console.ReadKey();
