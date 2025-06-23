8a358e9cb4dd8aded16eb4ef098c589a18f4cd2e
Muhammad Siddiqui <Muhammad.Siddiqui@civica.com.au>
2025-06-23 14:50:31 +1000

    Test project added

---
+++ Tests/Examples.Ferocia.TDCalculator.Tests/Services/CompoundInterestCalculatorServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Examples.Ferocia.TDCalculator.Domain.Services;
+using Examples.Ferocia.TDCalculator.Services.InterestServices;
+
+namespace Examples.Ferocia.TDCalculator.Tests.Services
+{
+    public class CompoundInterestCalculatorServiceTests
+    {
+        private ICompoundInterestCalculatorService _compoundInterestCalculatorService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _compoundInterestCalculatorService = new CompoundInterestCalculatorService();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
