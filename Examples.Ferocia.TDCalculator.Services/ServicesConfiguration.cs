f29a642fc9f1935edea1edd85eeb7e17ff5a3067
Muhammad Siddiqui <Muhammad.Siddiqui@civica.com.au>
2025-06-23 14:37:28 +1000

    standard DI for the service using Microsoft.Extensions.DependencyInjection

---
+++ Examples.Ferocia.TDCalculator.Services/ServicesConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Examples.Ferocia.TDCalculator.Domain.Services;
+using Examples.Ferocia.TDCalculator.Services.InterestServices;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.Ferocia.TDCalculator.Services
+{
+    public static class ServicesConfiguration
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services)
+        {
+            services.AddSingleton<ICompoundInterestCalculatorService, CompoundInterestCalculatorService>();
+            return services;
+        }
+    }
+}
